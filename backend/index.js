f2c34e4fdad4a66f303a337bef2851c947740a39
Anjali negi <113291155+Anjali4402@users.noreply.github.com>
2025-06-25 23:18:18 +0530

    add login api backend route

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,32 @@ app.use(express.json());
 
 app.post('/register', async (req, resp) => {
 
-    let user  = new User(req.body);
+    let user = new User(req.body);
     let result = await user.save();
 
+    result = result.toObject();
+
+    // it will not show the password in the result ( not show in the response) good practice.
+    delete result.password
+
     resp.send(result);
+});
+
+app.post('/login', async (req, resp) => {
+
+    if (req.body?.email && req.body?.password) {
+
+        // remove password for the sending in response
+        let user = await User.findOne(req.body).select("-password");
+
+        if (user) {
+            resp.send(user)
+        } else {
+            resp.send('No User Found')
+        }
+    } else {
+        resp.send("No result found")
+    }
 })
 
 
